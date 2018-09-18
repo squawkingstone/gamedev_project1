0ca8c75a7efe1fa798963743621b2398692a22ff
Lorelei Wright <wrighl@rpi.edu>
2018-09-18 19:48:39 -0400

    updated MovementState to use Level

--- source/MovementState.hx
+++ source/MovementState.hx
@@ -18,9 +18,7 @@ class MovementState extends FlxState
     var _player : Player;
     var _attack_object : FlxObject;
     var _cache : Cache;
-    var _background : FlxTilemap;
-    var _walls : FlxTilemap;
-    var _climb : FlxTilemap;
+    var _level : Level;
 
     var _zoom_level = 2;
 
@@ -30,24 +28,14 @@ class MovementState extends FlxState
 
         // LOAD THE MAP
 
+        _level = new Level("assets/maps/temp.tmx", AssetPaths.tiles__png);
+
         var tiled_map:TiledMap = new TiledMap("assets/maps/temp.tmx");
         FlxG.worldBounds.set(0, 0, tiled_map.fullWidth, tiled_map.fullHeight);
 
-        _background = new FlxTilemap();
-        var bg_layer:TiledTileLayer = cast(tiled_map.getLayer("Background"), TiledTileLayer);
-        _background.loadMapFromArray(bg_layer.tileArray, bg_layer.width, bg_layer.height, AssetPaths.tiles__png, tiled_map.tileWidth, tiled_map.tileHeight, OFF, 1, 1, 1);        
-        add(_background);
-
-        _walls = new FlxTilemap();
-        var wall_layer:TiledTileLayer = cast(tiled_map.getLayer("Walls"), TiledTileLayer);
-        _walls.loadMapFromArray(wall_layer.tileArray, 32, 16, AssetPaths.tiles__png, tiled_map.tileWidth, tiled_map.tileHeight, OFF, 1, 1, 3);
-        _walls.setTileProperties(2, FlxObject.ANY, null, null);
-        add(_walls);
-
-        _climb = new FlxTilemap();
-        var climb_layer:TiledTileLayer = cast(tiled_map.getLayer("Climbable"), TiledTileLayer);
-        _climb.loadMapFromArray(climb_layer.tileArray, climb_layer.width, climb_layer.height, AssetPaths.tiles__png, tiled_map.tileWidth, tiled_map.tileHeight, OFF, 1, 1, 1);
-        add(_climb);      
+        add(_level.get_background());
+        add(_level.get_walls());
+        add(_level.get_climb());   
 
         _player = new Player(24,24);
         add(_player);
@@ -67,9 +55,9 @@ class MovementState extends FlxState
     override public function update(elapsed:Float):Void
     {
         super.update(elapsed);
-        FlxG.collide(_walls, _player, null);
+        FlxG.collide(_level.get_walls(), _player, null);
         FlxG.collide(_cache, _player, null);
-        _player.process_movement(_climb.overlaps(_player, false, null));
+        _player.process_movement(_level.get_climb().overlaps(_player, false, null));
         var diff = (_player._facing == FlxObject.LEFT) ? -_player.width : _player.width;
         _attack_object.setPosition(_player.x + diff, _player.y);
         

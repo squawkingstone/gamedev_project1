fd37d8f60f7bfd430a079eb12e94ba0c6013775a
Lorelei Wright <wrighl@rpi.edu>
2018-09-18 15:37:30 -0400

    removed unnecessary comments

--- source/MovementState.hx
+++ source/MovementState.hx
@@ -47,9 +47,7 @@ class MovementState extends FlxState
         _climb = new FlxTilemap();
         var climb_layer:TiledTileLayer = cast(tiled_map.getLayer("Climbable"), TiledTileLayer);
         _climb.loadMapFromArray(climb_layer.tileArray, climb_layer.width, climb_layer.height, AssetPaths.tiles__png, tiled_map.tileWidth, tiled_map.tileHeight, OFF, 1, 1, 1);
-        add(_climb);
-
-        // DO THE PLAYER THING        
+        add(_climb);      
 
         _player = new Player(24,24);
         add(_player);
@@ -58,7 +56,6 @@ class MovementState extends FlxState
         _attack_object.allowCollisions = FlxObject.NONE;
         add(_attack_object);
 
-        // I should just add in a graphic thing and call it good for the test cache
         _cache = new Cache(16, 192);
         add(_cache);
 
@@ -76,23 +73,11 @@ class MovementState extends FlxState
         var diff = (_player._facing == FlxObject.LEFT) ? -_player.width : _player.width;
         _attack_object.setPosition(_player.x + diff, _player.y);
         
-        // check attack against every enemy
-
-        // check attack against every cache
+        //make a group of all the enemies and caches and use the attack as callback
         if (_attack_object.overlaps(_cache, false, null))
         {
             _player.process_attack(_cache);
         }
-        // I have to check this overlap with every enemy, then go from there
-        // I'll also have to check overlap with every cache in the game to see about that...
-
-        /*
-            So for the caching, then on justPressed, set some var to true, and start a counter counting
-            down, then, I just check each update if I'm still overlapping the cache and then continue
-            decrementing, otherwise, cancel the whole thing. Maybe I'll talk to the specific cache
-            object and call some function inside there so the thing can maintain a running track of it's
-            state, then the player could resume dissecting it after cancelling... I'll see
-        */
     }
 
 }

1bbbc771831a8493cdc685cf95109c13c3a223fb
Raegan <Raegan1414@gmail.com>
2018-09-20 19:23:04 -0400

    Added menu screen

--- source/Main.hx
+++ source/Main.hx
@@ -8,6 +8,6 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(160, 120, MovementState, 1.0, 60, 60, true, false));
+		addChild(new FlxGame(160, 120, MenuState, 1.0, 60, 60, true, false));
 	}
 }
---
+++ source/MenuState.hx
@@ -0,0 +1,34 @@
+package;
+
+import flixel.FlxState;
+import flixel.FlxSprite;
+import flixel.ui.FlxButton;
+import flixel.FlxG;
+
+class MenuState extends FlxState
+{
+    var startButton : FlxButton;
+    var background : FlxSprite;
+
+    override public function create():Void
+    {
+        background = new FlxSprite();
+        background.loadGraphic(AssetPaths.MenuImage__png);
+        add(background);
+        startButton = new FlxButton(20, 20, "START", clickPlay);
+        startButton.screenCenter();
+        add(startButton);
+        super.create();
+
+    }
+
+    override public function update(elapsed:Float):Void
+    {
+        super.update(elapsed);
+    }
+
+    function clickPlay():Void
+    {
+        FlxG.switchState(new MovementState());
+    }
+}

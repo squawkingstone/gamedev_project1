6eb741a35a531da63f7d3f6005e87355501e26df
Lorelei Wright <wrighl@rpi.edu>
2018-09-20 17:47:49 -0400

    Auto stash before merge of "develop" and "origin/develop"

--- source/Player.hx
+++ source/Player.hx
@@ -5,8 +5,6 @@ import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.system.FlxAssets.FlxGraphicAsset;
 
-// TIME IN HAXE IS IN SECONDS
-
 class Player extends FlxSprite
 {
     /* STATIC PROPERTIES */
@@ -28,12 +26,17 @@ class Player extends FlxSprite
     public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset)
     {
         super(X, Y, SimpleGraphic);
-        loadGraphic("assets/images/player.png", true, 16, 16);
+        loadGraphic("assets/images/animation.png", true, 64, 64);
         setFacingFlip(FlxObject.LEFT, true, false);
         setFacingFlip(FlxObject.RIGHT, false, false);
-        animation.add("walking", [0], 1, true, false, false);
-        animation.add("climbing", [1], 1, true, false, false);
-        animation.play("walking");
+        animation.add("jump", [0,1,2,3,4,5,6,7], 10, true, false, false); // maybe pause this???
+        animation.add("jump_attack", [10,11,12,13,14,15,16,17], 10, false, false, false);
+        animation.add("attack", [20,21,22,23,24,25,26,27,28], 10, false, false, false);
+        animation.add("walk", [30,31,32,33,34,35,36,37,38,39], 10, true, false, false);
+        animation.add("climb", [41,42,43,44,45,46], 10, true, false, false); // pause while still
+        animation.add("idle", [50,51,52,53], 10, true, false, false);
+        animation.add("death", [60,61,62], 10, false, false, false);
+        animation.play("idle");
         acceleration.y = _gravity;
     }
 
@@ -45,6 +48,9 @@ class Player extends FlxSprite
             var velx = 0.0;
             velx += (FlxG.keys.anyPressed(["RIGHT", "D"])) ? _climbing_speed : 0.0;
             velx -= (FlxG.keys.anyPressed(["LEFT", "A"])) ? _climbing_speed : 0.0;
+
+            if (velx > 0.0) { facing = FlxObject.RIGHT; }
+            if (velx < 0.0) { facing = FlxObject.LEFT; }
 
             var vely = 0.0;
             vely += (FlxG.keys.anyPressed(["DOWN", "S"])) ? _climbing_speed : 0.0;
@@ -61,14 +67,10 @@ class Player extends FlxSprite
             var velx = 0.0;
             velx += (FlxG.keys.anyPressed(["RIGHT", "D"])) ? _walking_speed : 0.0;
             velx -= (FlxG.keys.anyPressed(["LEFT", "A"])) ? _walking_speed : 0.0;
-            if (velx > 0.0) // this is bad, fix it later
-            { 
-                facing = FlxObject.RIGHT;
-            }
-            if (velx < 0.0) 
-            {
-                facing = FlxObject.LEFT;
-            }
+
+            if (velx > 0.0) { facing = FlxObject.RIGHT; }
+            if (velx < 0.0) { facing = FlxObject.LEFT; }
+
             velocity.x = velx;
             if (FlxG.keys.justPressed.SPACE && isTouching(FlxObject.FLOOR)) velocity.y = -_jump_speed;
             if ((FlxG.keys.justPressed.UP || FlxG.keys.justPressed.W) && climb_overlap) toggle_climbing(true, false);

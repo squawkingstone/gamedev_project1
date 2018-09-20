c8b97cf0355faae3de4ddd18fccc042d9e8e33a6
Raegan <Raegan1414@gmail.com>
2018-09-20 19:22:48 -0400

    Fixed tilesets

--- assets/tilesets/Brain.tsx
+++ assets/tilesets/Brain.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Brain" tilewidth="64" tileheight="64" tilecount="12" columns="2">
- <image source="Brain Tilesheet.png" width="128" height="384"/>
+ <image source="../../../../Brain Tilesheet.png" width="128" height="384"/>
 </tileset>

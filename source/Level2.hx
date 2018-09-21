e4780c5dbd96acabd5b8f70e5533b27cf17c9a89
Alexaa5 <43099240+Alexaa5@users.noreply.github.com>
2018-09-20 22:23:27 -0400

    Add files via upload

---
+++ source/Level2.hx
@@ -0,0 +1,24 @@
+package;
+
+import flixel.math.FlxPoint;
+
+class Level2 extends Level
+{
+    override public function create():Void
+    {
+        _file = "assets/maps/stomach.tmx";
+        _tilemap = AssetPaths.guts__png;
+        _player_start_position = new FlxPoint(1800, 1100);
+        
+        _monster_start_positions = new Array();
+        _monster_start_positions.push(new FlxPoint(70, 48));
+
+        _cache_start_positions = new Array();
+        _cache_start_positions.push(new FlxPoint(16, 192));
+
+        _next_level = new MovementState();
+        _level_time = 60000;
+
+        super.create();
+    }
+}

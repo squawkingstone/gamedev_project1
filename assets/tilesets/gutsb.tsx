99b6a1fecd8c2b6ea8e3146b4f42d0ea0ed93ed0
Alexaa5 <43099240+Alexaa5@users.noreply.github.com>
2018-09-20 21:51:08 -0400

    Add files via upload

---
+++ assets/tilesets/gutsb.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="guts" tilewidth="64" tileheight="64" tilecount="512" columns="32">
+ <image source="guts.png" width="2048" height="1024"/>
+</tileset>

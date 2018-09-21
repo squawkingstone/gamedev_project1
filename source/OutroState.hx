42f49b9929778f277b5106dcebf2bd2f1c940218
griffin <skutsg@rpi.edu>
2018-09-20 22:54:33 -0400

    New Game button

--- source/OutroState.hx
+++ source/OutroState.hx
@@ -5,10 +5,13 @@ import flixel.FlxGame;
 import haxe.Timer;
 import flixel.FlxG;
 import flixel.util.FlxSave;
+import flixel.ui.FlxButton;
 
 class OutroState extends FlxState
 {
     var scoreboard:FlxSave;
+    var newgameButton : FlxButton;
+
     override public function create():Void
     {
         scoreboard = new FlxSave();
@@ -19,10 +22,17 @@ class OutroState extends FlxState
         else var text = new flixel.text.FlxText(0, 0, 400, "You did not gather enough information for us to develop a vaccine. Humanity is doomed.", 16);
         text.screenCenter();
         add(text);
+        newgameButton = new FlxButton(280, 280, "Play Again?", clickNewGame);
+        add(newgameButton);
+
     }
 
     override public function update(elapsed:Float):Void
     {
         super.update(elapsed);
+    }
+    function clickNewGame():Void
+    {
+        FlxG.switchState(new MenuState());
     }
 }
